225079a29f5f684582853c03a3e954ccb2dea5f4
dkon70 <dkon70@inbox.ru>
2023-11-19 16:36:48 +0700

    refactor: remove unused api call scripts

--- src/scripts/getItem.ts
+++
@@ -1,7 +0,0 @@
-const getItem = async (id: number) => {
-  const response = await fetch(`https://dummyjson.com/products/${id}`);
-  const data = await response.json();
-  return data;
-};
-
-export default getItem;
