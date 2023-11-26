9bf9ac49f54a269bfeacdd949cb16698931a66ba
dkon70 <dkon70@inbox.ru>
2023-11-26 21:36:31 +0700

    feat: add card list component tests

---
+++ next/src/test/Main.test.tsx
@@ -0,0 +1,28 @@
+import { test, expect, describe } from 'vitest';
+import Main from '@/components/Main/Main';
+import { render, screen, waitFor } from '@testing-library/react';
+import { mockMultData } from './mockData';
+
+describe('Tests for the Card List component', () => {
+  test('renders the specified number of cards', async () => {
+    render(
+      <Main data={mockMultData} />
+    );
+
+    await waitFor(() => {
+      const cards = screen.getAllByTestId('card');
+      expect(cards.length).toBe(7);
+    });
+  });
+
+  test('appropriate message is displayed if no cards are present', async () => {
+    render(
+      <Main />
+    );
+
+    await waitFor(() => {
+      const message = screen.getByText('No Data');
+      expect(message).toBeInTheDocument();
+    });
+  });
+});
